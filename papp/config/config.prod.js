1d7ac696ad12239a788d9e57e967f753ae118940
RealNVS <133399110+RealNVS@users.noreply.github.com>
2024-10-26 12:28:23 +0530

    first commit

--- papp/config/config.prod.js
+++
@@ -1,8 +0,0 @@
-window.config = {
-    name: "#{USER_NAME}#",
-    title: "#{USER_TITLE}#",
-    bio: "#{USER_BIO}#",
-    location: "#{USER_LOCATION}#",
-    email: "#{USER_EMAIL}#",
-    image: "#{USER_IMAGE}#"
-  };
---
+++ papp/js/config.js
@@ -0,0 +1,8 @@
+window.config = {
+    name: 'Vishal Saran N',
+    title: 'Portfolio',
+    bio: 'Im Vishal Saran from Tirupur. Currently pursuing Computer Science Engineering Degree at Dr.N.G.P Institute of Technology',
+    location: 'Tirupur',
+    email: '22cs120@drngpit.ac.in',
+    image: '"C:\Users\visha\OneDrive\Desktop\saara .jpg"'
+};
